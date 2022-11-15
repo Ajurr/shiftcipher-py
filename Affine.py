43cd732e775b0ae24a9a6a3de921086f46572bb9
Hayden Hardaway <lytnaver@gmail.com>
2022-11-15 14:42:34 -0500

    Update Affine.py

--- Affine.py
+++ Affine.py
@@ -1,7 +1,7 @@
 '''
 Created on Feb 11, 2020
 
-@author: lytna
+@author: Hayden H
 '''
 alphabetUpper = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 alphabetLower = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
